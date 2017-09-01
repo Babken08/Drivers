d04312a2aea0fee57f5cec2a53ffede75ead0bdf
Babken Mxitaryan <babkenmxitaryan@gmail.com>
2017-09-01 11:48:47 +0400

    add structur

---
+++ DriversApplication/app/src/main/java/com/example/android/driversapplication/Activitys/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.driversapplication.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.driversapplication.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
